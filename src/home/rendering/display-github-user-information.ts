a8d12278ff4a0dbc75dbc78433e139c5d988e59a
zugdev <zug.dev@proton.me>
2024-11-01 01:34:16 -0300

    chore: lint

--- src/home/rendering/display-github-user-information.ts
+++ src/home/rendering/display-github-user-information.ts
@@ -34,7 +34,7 @@ export async function displayGitHubUserInformation(gitHubUser: GitHubUser) {
       renderErrorInModal(error, "Error logging out");
       alert("Error logging out");
     }
-    window.location.replace('/');
+    window.location.replace("/");
   });
 
   if (await isOrgMemberWithoutScope()) {
